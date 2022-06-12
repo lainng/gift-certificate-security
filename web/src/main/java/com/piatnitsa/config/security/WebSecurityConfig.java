41d6bc51cbf823243953f67e4baae60f58fc0b6a
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-12 21:34:28 +0300

    Update configure() method

--- web/src/main/java/com/piatnitsa/config/security/WebSecurityConfig.java
+++ web/src/main/java/com/piatnitsa/config/security/WebSecurityConfig.java
@@ -1,16 +1,27 @@
 package com.piatnitsa.config.security;
 
+import com.piatnitsa.jwt.JWTFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JWTFilter jwtFilter;
+
+    @Autowired
+    public WebSecurityConfig(JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,6 +30,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/tags/**").permitAll()
+                .antMatchers("/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/certificates/**").fullyAuthenticated()
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
