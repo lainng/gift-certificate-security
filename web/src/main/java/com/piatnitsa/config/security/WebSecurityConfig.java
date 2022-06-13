ab280eaa42b700aaffe0cad0421f6b3b082a2f67
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-13 21:33:02 +0300

    Added new restrictions for user roles

--- web/src/main/java/com/piatnitsa/config/security/WebSecurityConfig.java
+++ web/src/main/java/com/piatnitsa/config/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final JWTFilter jwtFilter;
+    private static final String ADMIN_ROLE = "ADMIN";
 
     @Autowired
     public WebSecurityConfig(JWTFilter jwtFilter) {
@@ -35,10 +37,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/tags/**").permitAll()
+                //All
+                .antMatchers(HttpMethod.GET, "/certificates/**").permitAll()
                 .antMatchers("/auth").permitAll()
-                .antMatchers(HttpMethod.GET, "/certificates/**").fullyAuthenticated()
+                //User
+                .antMatchers(HttpMethod.POST, "/orders/**").fullyAuthenticated()
+                .antMatchers(HttpMethod.GET, "/tags/**", "/users/**").fullyAuthenticated()
+                //Admin
+                .anyRequest().hasRole(ADMIN_ROLE)
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
