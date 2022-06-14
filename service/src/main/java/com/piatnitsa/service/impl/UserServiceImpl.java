6fc2ddefdacf0dde511eb2608c1151a98c5eb688
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-14 21:32:36 +0300

    Added insert() method

--- service/src/main/java/com/piatnitsa/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/piatnitsa/service/impl/UserServiceImpl.java
@@ -1,26 +1,35 @@
 package com.piatnitsa.service.impl;
 
-import com.piatnitsa.dao.UserDao;
+import com.piatnitsa.dao.CRDDao;
+import com.piatnitsa.dao.UserRepository;
+import com.piatnitsa.entity.Role;
 import com.piatnitsa.entity.User;
 import com.piatnitsa.service.AbstractService;
 import com.piatnitsa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 
 import java.util.List;
 
-@Component
+@Service
 public class UserServiceImpl extends AbstractService<User> implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao) {
-        super(userDao);
+    public UserServiceImpl(CRDDao<User> dao, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        super(dao);
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User insert(User entity) {
-        throw new UnsupportedOperationException();
+        entity.setPassword(passwordEncoder.encode(entity.getPassword()));
+        entity.setRole(Role.USER);
+        return userRepository.save(entity);
     }
 
     @Override
