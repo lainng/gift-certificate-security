3ee93bbd471707d14c7e4e2a64baa093be928c1c
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-14 21:31:11 +0300

    Added /register endpoint

--- web/src/main/java/com/piatnitsa/controller/AuthenticationController.java
+++ web/src/main/java/com/piatnitsa/controller/AuthenticationController.java
@@ -1,7 +1,12 @@
 package com.piatnitsa.controller;
 
 import com.piatnitsa.dto.UserCredentialsDto;
+import com.piatnitsa.dto.UserDto;
+import com.piatnitsa.dto.converter.DtoConverter;
+import com.piatnitsa.entity.User;
+import com.piatnitsa.hateoas.LinkBuilder;
 import com.piatnitsa.jwt.JWTProvider;
+import com.piatnitsa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,13 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
+    private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JWTProvider jwtProvider;
+    private final DtoConverter<UserDto, User> userDtoConverter;
+    private final LinkBuilder<UserDto> linkBuilder;
 
     @Autowired
-    public AuthenticationController(AuthenticationManager authenticationManager, JWTProvider jwtProvider) {
+    public AuthenticationController(UserService userService, AuthenticationManager authenticationManager,
+                                    JWTProvider jwtProvider, DtoConverter<UserDto, User> userDtoConverter,
+                                    LinkBuilder<UserDto> linkBuilder) {
+        this.userService = userService;
         this.authenticationManager = authenticationManager;
         this.jwtProvider = jwtProvider;
+        this.userDtoConverter = userDtoConverter;
+        this.linkBuilder = linkBuilder;
     }
 
     @PostMapping("/auth")
@@ -30,5 +43,14 @@ public class AuthenticationController {
         String token = jwtProvider.generateToken(userCredentialsDto.getEmail());
         userCredentialsDto.setToken(token);
         return userCredentialsDto;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto registerUser(@RequestBody UserDto userDto) {
+        User savedUser = userService.insert(userDtoConverter.toEntity(userDto));
+        UserDto savedUserDto = userDtoConverter.toDto(savedUser);
+        linkBuilder.buildLinks(savedUserDto);
+        return savedUserDto;
     }
 }
