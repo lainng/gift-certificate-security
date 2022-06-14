2e268204ffad367382e50face2cd4ddb3b747ef7
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-14 19:57:21 +0300

    Added new constructors

--- web/src/main/java/com/piatnitsa/exception/ErrorResponse.java
+++ web/src/main/java/com/piatnitsa/exception/ErrorResponse.java
@@ -9,6 +9,14 @@ public class ErrorResponse {
     private String errorCode;
     private String errorMessage;
 
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(String errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
     public String getErrorCode() {
         return errorCode;
     }
