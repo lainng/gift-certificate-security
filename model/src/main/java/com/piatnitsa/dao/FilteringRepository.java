df75405f352dcc5382b529b323b795b6f49f817b
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-15 21:55:14 +0300

    Renamed repository

---
+++ model/src/main/java/com/piatnitsa/dao/FilteringRepository.java
@@ -0,0 +1,23 @@
+package com.piatnitsa.dao;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+
+/**
+ * This interface describes CRD operations for working with database tables.
+ * @param <T> type of entity.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+public interface FilteringRepository<T> {
+
+    /**
+     * Method for getting a list of {@link T} by specific parameters.
+     * @param params request parameters from URL.
+     * @return {@link List} of {@link T}.
+     */
+    List<T> findWithFilter(MultiValueMap<String, String> params, Pageable pageable);
+}
