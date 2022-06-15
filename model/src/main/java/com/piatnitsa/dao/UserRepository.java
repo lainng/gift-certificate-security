3f39eccda562259c7cff9ca77aeb32a5415c4076
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-15 12:22:50 +0300

    Update javadoc

--- model/src/main/java/com/piatnitsa/dao/UserRepository.java
+++ model/src/main/java/com/piatnitsa/dao/UserRepository.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
+/**
+ * This interface describes abstract behavior for working with <code>user</code> table in the database.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
 @Repository
 @Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
