a819e3840bed1f45a58fec2839862f84506da2cf
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-20 22:04:56 +0300

    Update javadoc

--- web/src/main/java/com/piatnitsa/dto/UserCredentialsDto.java
+++ web/src/main/java/com/piatnitsa/dto/UserCredentialsDto.java
@@ -2,6 +2,11 @@ package com.piatnitsa.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * This class represents DTO that contains the user's credentials.
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
 public class UserCredentialsDto {
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
